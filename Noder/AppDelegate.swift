e8ca5b64279d1a40ee22352bb5d5a031dd3a17b1
Kathelyss <rubicante@icloud.com>
2018-05-23 20:07:30 +0300

    Initial Commit

---
+++ Noder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Noder
+//
+//  Created by Екатерина Рыжова on 23.05.2018.
+//  Copyright © 2018 Екатерина Рыжова. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Noder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Noder
+//
+//  Created by Екатерина Рыжова on 23.05.2018.
+//  Copyright © 2018 Екатерина Рыжова. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
