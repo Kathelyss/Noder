56f349c2ccb9693ce9052438749387509f7ecc57
Kathelyss <rubicante@icloud.com>
2018-05-25 11:05:23 +0300

    create main view and add properties

--- Noder/ViewController.swift
+++ Noder/ViewController.swift
@@ -9,7 +9,21 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet var indexTextField: NSTextField!
+    @IBOutlet var nameTextField: NSTextField!
+    @IBOutlet var xTextField: NSTextField!
+    @IBOutlet var yTextField: NSTextField!
+    @IBOutlet var floorTextField: NSTextField!
+    @IBOutlet var connectionNodeTextField: NSTextField!
+    @IBOutlet var edgeLength: NSTextField!
+    @IBOutlet var edgeWeight: NSTextField!
+    
+    @IBOutlet var tableView: NSTableView!
+    
+    @IBAction func tapAddNode(_ sender: NSButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
